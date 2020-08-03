ac4d270df48fb3679630699034865a168be642ce
KarachunVladislav01 <karachunvladislav@gmail.com>
2020-08-03 05:10:55 +0300

    Updates

---
+++ precache-manifest.ab0399d213274f4782f6a6e4d4460b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f61b87655f020c4eb305343da609ce",
+    "url": "/Instagram-NASA/index.html"
+  },
+  {
+    "revision": "c8da17210d75749baa4a",
+    "url": "/Instagram-NASA/static/css/main.b8ac7825.chunk.css"
+  },
+  {
+    "revision": "c0f347e789204187f3c0",
+    "url": "/Instagram-NASA/static/js/2.7b2cfd99.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Instagram-NASA/static/js/2.7b2cfd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8da17210d75749baa4a",
+    "url": "/Instagram-NASA/static/js/main.2d37cc6a.chunk.js"
+  },
+  {
+    "revision": "845a2b428b18fe681142",
+    "url": "/Instagram-NASA/static/js/runtime-main.49f909e1.js"
+  }
+]);
